afb9ea37215682bf0582cb27fed7b421157870be
Jason Lv <jasonlvhit@gmail.com>
2014-07-17 00:27:20 +0800

    追加lambda的一些片段

---
+++ cpp1x/lambda.cpp
@@ -0,0 +1,12 @@
+#include "lambda.h"
+
+int main(){
+	basic();
+	capture();
+	assign();
+	nest();
+	execption();
+	negate();
+	getchar();
+	return 0;
+}
--- cpp1x/lambda.h
+++ cpp1x/lambda.h
@@ -1,3 +1,9 @@
+/*
+* Reference£º
+* http://msdn.microsoft.com/en-us/library/dd293608.aspx
+* 
+*/
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -21,10 +27,55 @@ vector<int> initSTL(){
 void capture(){
 	size_t count = 0;
 	vector<int> v = initSTL();
-	for_each(v.begin(), v.end(), ][&count](int value){
+	for_each(v.begin(), v.end(), [&count](int value){
 		if (value % 2) count++;
 	});
 }
+
+//basic assign lambda expression to the function object
+void assign(){
+	auto f = [](int x, int y){ return x + y; };
+	cout << f(1, 3) << endl;
+}
+
+//nested lambda expression
+void nest(){
+	auto nested = [](int x){ return [](int y) { return y * 2; }(x)+3;  };
+	cout << nested(5) << endl;
+}
+
+//catch exception
+void execption(){
+	vector<int> elem(3);
+	vector<int> index(3);
+	index[0] = 0;
+	index[1] = -1;
+	index[2] = 2;
+	try{
+		for_each(index.begin(), index.end(), [&](int index_){
+			elem.at(index_) = index_;
+		});
+	}catch (const out_of_range& e){
+		cerr << "Caught '" << e.what() << "'." << endl;
+	}
+}
+
+//template
+template<typename T>
+void templates_negate(vector<T> &v)
+{
+	for_each(v.begin(), v.end(), [](T& t){ t = -t; });
+}
+
+void negate()
+{
+	vector<int> v(3, 2);
+	for_each(v.begin(), v.end(), [](int i){cout << i << endl; });
+	templates_negate(v);
+	for_each(v.begin(), v.end(), [](int i){cout << i << endl; });
+}
+
+
 
 
 
